07766999224512727c8aa0216d6b4027904ff612
yequan <yequan_zhao@ucsb.edu>
2026-05-27 12:04:44 -0700

    feat(peft): add PEFT_MODE driven $PEFT_ARGS to grpo.sh / OPD launcher

--- on_policy_distillation.sh
+++ on_policy_distillation.sh
@@ -28,6 +28,9 @@ if [ -z "$SLURM_JOB_ID" ]; then
     echo "=========================================="
 fi
 
+# Make compress/ package importable when PEFT_MODE=blocktt|svd is enabled.
+export PYTHONPATH="$(dirname "$(realpath "$0")")/src${PYTHONPATH:+:$PYTHONPATH}"
+
 ray stop --force
 export RAY_memory_usage_threshold=0.99
 export CUDA_LAUNCH_BLOCKING=${CUDA_LAUNCH_BLOCKING:-1}
@@ -149,7 +152,7 @@ export SAVE_FREQ=${SAVE_FREQ:-20}
 export TEST_FREQ=${TEST_FREQ:-20}
 export TOTAL_EPOCHS=${TOTAL_EPOCHS:-1}
 export REWARD_MICRO_BATCH_SIZE_PER_GPU=${REWARD_MICRO_BATCH_SIZE_PER_GPU:-24}
-export CKPT_PATH=${PROJECT_PATH}/${ADV_ESTIMATOR}_${TRAIN_DATASET_NAME}_${ACTOR_MODEL_NAME}_${REWARD_MODEL_NAME}_${MAX_RESP_LENGTH}-T_${TEMPERATURE}-Tch_${TEACHER_TEMPERATURE}-n_${N_RESPONSES}-mbs_${MINI_BATCH_SIZE}-topk_${LOG_PROB_TOP_K}-topk_strategy_${TOP_K_STRATEGY}-rw_${REWARD_WEIGHT_MODE}-$(date +%Y-%m-%d_%H-%M-%S)
+export CKPT_PATH=${PROJECT_PATH}/${ADV_ESTIMATOR}_${TRAIN_DATASET_NAME}_${ACTOR_MODEL_NAME}_${REWARD_MODEL_NAME}_${MAX_RESP_LENGTH}-T_${TEMPERATURE}-Tch_${TEACHER_TEMPERATURE}-n_${N_RESPONSES}-mbs_${MINI_BATCH_SIZE}-topk_${LOG_PROB_TOP_K}-topk_strategy_${TOP_K_STRATEGY}-rw_${REWARD_WEIGHT_MODE}_peft-${PEFT_MODE:-none}-$(date +%Y-%m-%d_%H-%M-%S)
 export OUTLINES_CACHE_DIR=~/.cache/outlines/$(uuidgen)
 export NCCL_DEBUG=WARN
 
@@ -160,7 +163,7 @@ export SWANLAB_LOG_DIR=${PROJECT_PATH}/swanlab_log
 export HYDRA_FULL_ERROR=1
 
 
-export EXPERIMENT_NAME=${ADV_ESTIMATOR}_${TRAIN_DATASET_NAME}_${ACTOR_MODEL_NAME}_${REWARD_MODEL_NAME}_${MAX_RESP_LENGTH}-T_${TEMPERATURE}-Tch_${TEACHER_TEMPERATURE}-n_${N_RESPONSES}-mbs_${MINI_BATCH_SIZE}-topk_${LOG_PROB_TOP_K}-topk_strategy_${TOP_K_STRATEGY}-rw_${REWARD_WEIGHT_MODE}-$(date +%Y-%m-%d_%H-%M-%S)
+export EXPERIMENT_NAME=${ADV_ESTIMATOR}_${TRAIN_DATASET_NAME}_${ACTOR_MODEL_NAME}_${REWARD_MODEL_NAME}_${MAX_RESP_LENGTH}-T_${TEMPERATURE}-Tch_${TEACHER_TEMPERATURE}-n_${N_RESPONSES}-mbs_${MINI_BATCH_SIZE}-topk_${LOG_PROB_TOP_K}-topk_strategy_${TOP_K_STRATEGY}-rw_${REWARD_WEIGHT_MODE}_peft-${PEFT_MODE:-none}-$(date +%Y-%m-%d_%H-%M-%S)
 
 KL_ARGS=""
 if [ "$USE_KL" = "True" ]; then
@@ -183,6 +186,89 @@ echo "PPO_MAX_TOKEN_LEN_PER_GPU: $PPO_MAX_TOKEN_LEN_PER_GPU"
 
 ray start --head
 sleep 5
+
+
+# ---- PEFT ----
+export PEFT_MODE=${PEFT_MODE:-none}
+export PEFT_TARGET_MODULES=${PEFT_TARGET_MODULES:-all}
+
+export LORA_RANK=${LORA_RANK:-16}
+export LORA_ALPHA=${LORA_ALPHA:-32}
+export LORA_DROPOUT=${LORA_DROPOUT:-0.0}
+
+export QLORA_QUANT_TYPE=${QLORA_QUANT_TYPE:-nf4}
+export QLORA_DOUBLE_QUANT=${QLORA_DOUBLE_QUANT:-True}
+export QLORA_COMPUTE_DTYPE=${QLORA_COMPUTE_DTYPE:-bfloat16}
+
+export BTT_DECOMP_MODE=${BTT_DECOMP_MODE:-input_one_block}
+export BTT_RANK=${BTT_RANK:-full}
+export BTT_TRAIN_POSITION=${BTT_TRAIN_POSITION:-small}
+export BTT_S_MERGED_TO=${BTT_S_MERGED_TO:-frozen}
+export BTT_CONVERT_MODE=${BTT_CONVERT_MODE:-svd}
+export BTT_FACTORIZE_BY_HEAD=${BTT_FACTORIZE_BY_HEAD:-True}
+export BTT_NORMALIZE_AFTER_UPDATE=${BTT_NORMALIZE_AFTER_UPDATE:-False}
+export BTT_QFURA=${BTT_QFURA:-False}
+
+export SVD_TRAIN_POSITION=${SVD_TRAIN_POSITION:-output}
+export SVD_S_MERGED_TO=${SVD_S_MERGED_TO:-frozen}
+export SVD_COMPRESSION_RATIO=${SVD_COMPRESSION_RATIO:-1.0}
+
+export CALIB_MODE=${CALIB_MODE:-none}
+export CALIB_SOURCE=${CALIB_SOURCE:-c4}
+export CALIB_NUM_SEQS=${CALIB_NUM_SEQS:-128}
+export CALIB_MAX_LENGTH=${CALIB_MAX_LENGTH:-2048}
+export CALIB_BATCH_SIZE=${CALIB_BATCH_SIZE:-8}
+export CALIB_SEED=${CALIB_SEED:-3}
+export CALIB_TRACES_PATH=${CALIB_TRACES_PATH:-}
+
+PEFT_ARGS="+actor_rollout_ref.peft.mode=$PEFT_MODE \
++actor_rollout_ref.peft.target_modules=$PEFT_TARGET_MODULES"
+
+case "$PEFT_MODE" in
+  none) ;;
+  lora)
+    PEFT_ARGS="$PEFT_ARGS \
+      +actor_rollout_ref.peft.lora.rank=$LORA_RANK \
+      +actor_rollout_ref.peft.lora.alpha=$LORA_ALPHA \
+      +actor_rollout_ref.peft.lora.dropout=$LORA_DROPOUT" ;;
+  qlora)
+    PEFT_ARGS="$PEFT_ARGS \
+      +actor_rollout_ref.peft.lora.rank=$LORA_RANK \
+      +actor_rollout_ref.peft.lora.alpha=$LORA_ALPHA \
+      +actor_rollout_ref.peft.qlora.bnb_4bit_quant_type=$QLORA_QUANT_TYPE \
+      +actor_rollout_ref.peft.qlora.bnb_4bit_use_double_quant=$QLORA_DOUBLE_QUANT \
+      +actor_rollout_ref.peft.qlora.bnb_4bit_compute_dtype=$QLORA_COMPUTE_DTYPE" ;;
+  blocktt)
+    PEFT_ARGS="$PEFT_ARGS \
+      +actor_rollout_ref.peft.blocktt.decomp_mode=$BTT_DECOMP_MODE \
+      +actor_rollout_ref.peft.blocktt.rank=$BTT_RANK \
+      +actor_rollout_ref.peft.blocktt.train_position=$BTT_TRAIN_POSITION \
+      +actor_rollout_ref.peft.blocktt.s_merged_to=$BTT_S_MERGED_TO \
+      +actor_rollout_ref.peft.blocktt.convert_mode=$BTT_CONVERT_MODE \
+      +actor_rollout_ref.peft.blocktt.factorize_by_head=$BTT_FACTORIZE_BY_HEAD \
+      +actor_rollout_ref.peft.blocktt.normalize_after_update=$BTT_NORMALIZE_AFTER_UPDATE \
+      +actor_rollout_ref.peft.blocktt.qfura.enabled=$BTT_QFURA" ;;
+  svd)
+    PEFT_ARGS="$PEFT_ARGS \
+      +actor_rollout_ref.peft.svd.train_position=$SVD_TRAIN_POSITION \
+      +actor_rollout_ref.peft.svd.s_merged_to=$SVD_S_MERGED_TO \
+      +actor_rollout_ref.peft.svd.compression_ratio=$SVD_COMPRESSION_RATIO" ;;
+  *) echo "Unknown PEFT_MODE=$PEFT_MODE" >&2; exit 1 ;;
+esac
+
+if [ "$CALIB_MODE" != "none" ]; then
+  PEFT_ARGS="$PEFT_ARGS \
+    +actor_rollout_ref.peft.calib.mode=$CALIB_MODE \
+    +actor_rollout_ref.peft.calib.source=$CALIB_SOURCE \
+    +actor_rollout_ref.peft.calib.num_seqs=$CALIB_NUM_SEQS \
+    +actor_rollout_ref.peft.calib.max_length=$CALIB_MAX_LENGTH \
+    +actor_rollout_ref.peft.calib.batch_size=$CALIB_BATCH_SIZE \
+    +actor_rollout_ref.peft.calib.seed=$CALIB_SEED"
+  if [ -n "$CALIB_TRACES_PATH" ]; then
+    PEFT_ARGS="$PEFT_ARGS +actor_rollout_ref.peft.calib.traces_path=$CALIB_TRACES_PATH"
+  fi
+fi
+# ---- /PEFT ----
 
 
 python3 -m verl.trainer.main_ppo \
@@ -260,6 +346,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.total_epochs=$TOTAL_EPOCHS \
     trainer.default_local_dir="$CKPT_PATH" \
     trainer.is_plot=$IS_PLOT \
+    $PEFT_ARGS
 
 # Log the end time for local runs.
 if [ -z "$SLURM_JOB_ID" ]; then
